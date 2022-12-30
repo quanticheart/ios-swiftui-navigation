ba62244f4b903dd18eccda5b6cd8df467a7e68ea
Jonn Alves <jonatas.alsantos@jsl.com.br>
2022-12-30 12:04:38 -0300

    Initial Commit

---
+++ amiibo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  amiibo
+//
+//  Created by Jonn Alves on 30/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ amiibo/amiiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  amiiboApp.swift
+//  amiibo
+//
+//  Created by Jonn Alves on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct amiiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
